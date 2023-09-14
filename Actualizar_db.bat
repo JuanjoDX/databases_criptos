9ec2acedf0ba938359c447f78e0eab96ccbe6067
JuanjoDX <juanjoposada182@hotmail.com>
2023-09-13 23:33:35 -0500

    cambio en la ruta

--- Actualizar_db.bat
+++ Actualizar_db.bat
@@ -1,13 +1,13 @@
 @echo off
 
 :: Ruta al directorio del entorno virtual
-set "venv_path=C:\Users\Usuario\Proyectos\BD_Monitoreo\venv"
+set "venv_path=C:\Users\juanj\Proyectos\databases_criptos\venv"
 
 :: Activa el entorno virtual
 call "%venv_path%\Scripts\activate"
 
 :: Ruta al archivo Python que deseas ejecutar
-set "python_script=C:\Users\Usuario\Proyectos\BD_Monitoreo\src\hist_poo\act_db_poo.py"
+set "python_script=C:\Users\juanj\Proyectos\databases_criptos\src\hist_poo\act_db_poo.py"
 
 :: Ejecuta el programa de Python
 python "%python_script%"
