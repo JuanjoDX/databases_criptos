901253f6012cf6e4d7904b9cacb8de69dff6926c
JuanjoDX <juanjoposada182@hotmail.com>
2023-09-13 23:23:33 -0500

    Se crean los .bat de la carpeta auto_bat para luego crear una tarea y hacer ejecuciones programadas

---
+++ src/auto_bat/act_db_8h.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:: Ruta al directorio del entorno virtual
+set "venv_path=C:\Users\juanj\Proyectos\databases_criptos\venv"
+
+:: Activa el entorno virtual
+call "%venv_path%\Scripts\activate"
+
+:: Ruta al archivo Python que deseas ejecutar
+set "python_script=C:\Users\juanj\Proyectos\databases_criptos\src\hist_poo\act_db_poo.py"
+
+:: Define la variable que deseas pasar al script de Python
+set "temporalidades=8h" 
+
+:: Ejecuta el programa de Python con la variable como argumento
+python "%python_script%" %temporalidades%
+
+:: Desactiva el entorno virtual
+call "%venv_path%\Scripts\deactivate"
+
+:: Pausa la ejecución para mantener la ventana abierta
+pause
