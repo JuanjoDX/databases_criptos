b490a86b35acf42ff13ebcd35abf2e8bc5009c5f
JuanjoDX <juanjoposada182@hotmail.com>
2023-09-20 21:38:56 -0500

    Se cambian archivos .bat de rutas absolutas a relativas

--- src/auto_bat/act_db_5m.bat
+++ src/auto_bat/act_db_5m.bat
@@ -1,13 +1,13 @@
 @echo off
 
 :: Ruta al directorio del entorno virtual
-set "venv_path=C:\Users\juanj\Proyectos\databases_criptos\venv"
+set "venv_path=.\venv"
 
 :: Activa el entorno virtual
 call "%venv_path%\Scripts\activate"
 
 :: Ruta al archivo Python que deseas ejecutar
-set "python_script=C:\Users\juanj\Proyectos\databases_criptos\src\hist_poo\act_db.py"
+set "python_script=.\src\hist_poo\act_db.py"
 
 :: Define la variable que deseas pasar al script de Python
 set "temporalidades=5m" 
